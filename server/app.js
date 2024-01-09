51ad557657ec4c34cfeeaef1e5931c6ac51f26e2
LeoSebastian23 <leosebastiang19@gmail.com>
2024-01-08 21:53:04 -0300

    update package.json and server

--- server/app.js
+++ server/app.js
@@ -2,9 +2,12 @@
 import express from 'express'
 import fileUpload from 'express-fileupload'
 import productsRoutes from './routes/products.routes.js'
-
+import path from 'path'
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express()
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // middlewares
 app.use(express.json())
@@ -16,5 +19,7 @@ app.use(fileUpload({
 
 // routes
 app.use(productsRoutes)
+console.log(__dirname) 
+app.use(express.static(path.join(__dirname, '../client01/build')));
 
 export default app
