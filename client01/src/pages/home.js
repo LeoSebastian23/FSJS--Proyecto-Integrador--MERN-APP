ba2ce7a245fe9f3a2a0e7d4764dbab181af39069
LeoSebastian23 <leosebastiang19@gmail.com>
2024-01-11 23:51:56 -0300

    update

--- client01/src/pages/home.js
+++ client01/src/pages/home.js
@@ -28,18 +28,19 @@ export function Home() {
 
   return (
     <div className="text-center my-8 flex flex-col items-center">
-      <h1 className="font-semibold text-5xl text-slate-50">
+      <h1 className="font-light text-5xl text-slate-50 shadow-1">
         {" "}
-        GALERIA DE PUBLICACIONES{" "}
+        FOTOGALERIA{" "}
       </h1>
+      <p className="font-light text-2xl mt-4 text-slate-200 shadow-1">Utiliza este espacio para compartir tu publicacion con todo el mundo.</p>
       <Link
         to="/catalogo"
-        className="relative text-slate-200 my-8 px-8 py-2 rounded-xl font-semibold text-xl bg-slate-700 isolation-auto z-10 border-2 border-amber-400 before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-left-full before:hover:left-0 before:rounded-full before:bg-amber-400 before:-z-10 before:aspect-square before:hover:scale-150 overflow-hidden before:hover:duration-700 hover:text-slate-800 hover:duration-700"
+        className="relative text-slate-200 font-light my-8 px-8 py-2 rounded-xl text-xl bg-slate-700 isolation-auto z-10 border-2 border-amber-400 before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-left-full before:hover:left-0 before:rounded-full before:bg-amber-400 before:-z-10 before:aspect-square before:hover:scale-150 overflow-hidden before:hover:duration-700 hover:text-slate-800 hover:duration-700"
       >
         {" "}
         Ingresar Publicación
       </Link>
-      <h1 className="font-semibold text-2xl text-slate-50">
+      <h1 className="font-light text-2xl text-slate-50">
         {" "}
         PUBLICACIONES ({release.length}){" "}
       </h1>
