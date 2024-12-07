cf808ff889d1812e8062e5ff3c1a0006bc80c6bd
LeoSebastian23 <leosebastiang19@gmail.com>
2024-12-07 03:32:49 -0300

    Update

--- frontend/src/api/products.js
+++ frontend/src/api/products.js
@@ -1,24 +1,23 @@
-import axios from 'axios'
+const BACKEND_URL = 'https://backend-mern-app-0kdp.onrender.com'; // Dirección de tu backend en producción
 
-export const getProductsRequests =  async() => await axios.get('/products')
+export const getProductsRequests =  async() => await axios.get(`${BACKEND_URL}/products`)
 
-export const createProductsRequests = async (product) =>{
+export const createProductsRequests = async (product) => {
+  const form = new FormData()
 
-const form = new FormData()
+  for(let key in product){
+    form.append(key, product[key])
+  }
 
-for(let key in product){
-    form.append(key,product[key])
-}
-
-return await axios.post('/products', form, {
-    headers:{
-        "Content-Type": "multipart/form-data"
+  return await axios.post(`${BACKEND_URL}/products`, form, {
+    headers: {
+      "Content-Type": "multipart/form-data"
     }
- });
+  });
 }
 
-export const deleteProductRequests = async id => await axios.delete('/products/' + id)
+export const deleteProductRequests = async id => await axios.delete(`${BACKEND_URL}/products/${id}`)
 
-export const getProductRequest = async(id) => await axios.get('/products/' + id)
+export const getProductRequest = async(id) => await axios.get(`${BACKEND_URL}/products/${id}`)
 
-export const updateProductRequest = async (id, product) => await axios.put(`/products/${id}`, product) 
+export const updateProductRequest = async (id, product) => await axios.put(`${BACKEND_URL}/products/${id}`, product)
