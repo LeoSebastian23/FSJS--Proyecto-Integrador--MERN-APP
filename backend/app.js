79e9a5186afdb918ec95aab6734604930de0de6b
LeoSebastian23 <leosebastiang19@gmail.com>
2024-12-07 02:33:39 -0300

    Up

--- backend/app.js
+++ backend/app.js
@@ -1,29 +1,33 @@
 // Configuracion de Express
-import express from 'express'
-import fileUpload from 'express-fileupload'
-import productsRoutes from './routes/products.routes.js'
-import path from 'path'
+import express from 'express';
+import fileUpload from 'express-fileupload';
+import productsRoutes from './routes/products.routes.js';
+import path, { join } from 'path'; // Importar 'join' desde 'path'
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
-const app = express()
+const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-// middlewares
-app.use(express.json())
-app.use(fileUpload({
+// Middlewares
+app.use(express.json());
+app.use(
+  fileUpload({
     useTempFiles: true,
-    tempFileDir: './upload'
-}))
+    tempFileDir: './upload',
+  })
+);
 
+// Routes
+app.use(productsRoutes);
 
-// routes
-app.use(productsRoutes)
-// console.log(__dirname) 
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+// Servir archivos estáticos del frontend
+app.use(express.static(join(__dirname, '../frontend/build')));
 
-app.get('*',(req,res)=> {
-    res.sendFile(join(__dirname,'..frontend/build/index.html'))
-})
+// Manejar rutas desconocidas para aplicaciones SPA (Single Page Application)
+app.get('*', (req, res) => {
+  res.sendFile(join(__dirname, '../frontend/build/index.html')); // Ruta corregida
+});
 
-export default app
+export default app;
+
